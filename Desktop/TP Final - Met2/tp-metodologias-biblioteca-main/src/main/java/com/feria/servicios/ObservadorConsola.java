d6dbbfadd6322790d44712caca1c53fcbf38ff5b
Facu-bit <facufredes2@gmail.com>
2026-05-29 13:46:01 -0300

    Delete Desktop/TP Final - Met2/tp-metodologias-biblioteca-main directory

--- Desktop/TP Final - Met2/tp-metodologias-biblioteca-main/src/main/java/com/feria/servicios/ObservadorConsola.java
+++
@@ -1,9 +0,0 @@
-package com.feria.servicios;
-
-public class ObservadorConsola implements ObservadorFeria {
-
-    @Override
-    public void actualizar(String evento, String detalle) {
-        System.out.println("[" + evento + "] " + detalle);
-    }
-}
--- Desktop/TP Final - Met2/tp-metodologias-biblioteca-main/src/main/java/com/feria/servicios/ObservadorFeria.java
+++
@@ -1,5 +0,0 @@
-package com.feria.servicios;
-
-public interface ObservadorFeria {
-    void actualizar(String evento, String detalle);
-}
